013e3274a15f55d4821ec41df7a43eaa58bd23c6
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-13 14:07:13 +0530

    all tasks page created

--- 07_notes_app_with_local_storage/src/components/AllTasks.jsx
+++ 07_notes_app_with_local_storage/src/components/AllTasks.jsx
@@ -3,11 +3,17 @@ function Alltasks() {
     <>
       <div className="h-screen">
         <h1 className="text-3xl font-bold px-10 py-4">All Tasks</h1>
+        <button className="bg-black px-10 py-3 mx-10 rounded-md cursor-pointer active:scale-95 transition">Add tasks</button>
         <div className="flex flex-wrap gap-5 p-10">
-          <div className="bg-zinc-900 h-40 w-[32%] flex flex-col flex-wrap justify-between rounded-md p-4">
-            <div>
-              <h1>Title</h1>
-              <p>Description</p>
+          <div className="bg-zinc-900 h-40 flex flex-col flex-wrap justify-between rounded-md p-4 w-[32%]">
+            <div className="flex gap-5">
+              <div className="h-20 w-20">
+                <img className="h-full w-full object-cover rounded-md shadow-gray-600 shadow-2xl" src="https://images.unsplash.com/photo-1786381624438-da16f6cfb834?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D" alt="alt" />
+              </div>
+              <div>
+                <h1 className="text-lg">Title: Lorem ipsum dololit.</h1>
+                <p className="text-wrap">Description: Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+              </div>
             </div>
             <div className="flex justify-between">
               <button className="bg-blue-300 text-blue-800 px-6 py-1 rounded-md cursor-pointer active:scale-95 transition">
--- 07_notes_app_with_local_storage/src/components/InputFields.jsx
+++ 07_notes_app_with_local_storage/src/components/InputFields.jsx
@@ -4,16 +4,30 @@ function InputFields() {
       <div className="h-screen flex flex-col justify-center items-center gap-10">
         <div className="p-4 rounded-md bg-zinc-900">
           <h1 className="text-3xl font-bold mb-10">Enter tasks</h1>
-          <div className="flex flex-col gap-5">
-            <input
-              type="text"
-              placeholder="Enter task"
-              className="border border-gray-700 shadow shadow-2xl outline-none px-6 py-4 rounded-md"
-            />
-            <textarea
-              placeholder="Enter task details"
-              className="border border-gray-700 shadow shadow-2xl resize-none w-2xl outline-none px-6 py-4 rounded-md"
-            ></textarea>
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Task name</label>
+              <input
+                type="text"
+                placeholder="Enter task"
+                className="border border-gray-700 shadow shadow-2xl outline-none px-6 py-4 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Image URL</label>
+              <input
+                type="text"
+                placeholder="Enter Image URL"
+                className="border border-gray-700 shadow shadow-2xl outline-none px-6 py-4 rounded-md"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Task description</label>
+              <textarea
+                placeholder="Enter task details"
+                className="border border-gray-700 shadow shadow-2xl resize-none w-2xl outline-none px-6 py-4 rounded-md"
+              ></textarea>
+            </div>
             <button
               type="submit"
               className="bg-black p-4 rounded-md active:scale-95 transition cursor-pointer"
