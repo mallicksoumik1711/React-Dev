feca4070d9ff02c7561198722bd48fb3be94e616
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-14 11:36:08 +0530

    folder structure

--- react_practice_problems/src/App.jsx
+++ react_practice_problems/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Counter from "./practice_problems/Counter";
+import Counter from "./practice_problems/01_counter_dynamic_classes/Counter";
 
 function App() {
   return (
