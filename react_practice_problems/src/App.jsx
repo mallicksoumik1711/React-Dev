0de5a284f7201dc056cc417a868fd5d301f10d5f
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-14 20:22:51 +0530

    digital clock

--- react_practice_problems/src/App.jsx
+++ react_practice_problems/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import ObjectManipulation from "./practice_problems/05_like_objects_manipulation/ObjectManipulation";
+import DigitalClock from "./practice_problems/06_digital_clock/DigitalClock";
+// import ObjectManipulation from "./practice_problems/05_like_objects_manipulation/ObjectManipulation";
 // import Todo from "./practice_problems/04_todo_notes_app/Todo";
 // import InputCharacterCounter from "./practice_problems/03_input_character_counter/InputCharacterCounter";
 // import ButtonToggle from "./practice_problems/02_button_toggle_show_hide/ButtonToggle";
@@ -12,7 +13,8 @@ function App() {
       {/* <ButtonToggle /> */}
       {/* <InputCharacterCounter /> */}
       {/* <Todo /> */}
-      <ObjectManipulation />
+      {/* <ObjectManipulation /> */}
+      <DigitalClock />
     </>
   );
 }
---
+++ react_practice_problems/src/practice_problems/06_digital_clock/DigitalClock.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function DigitalClock() {
+
+const [time, setTime] = useState(new Date())
+// console.log(time.getHours(), time.getMinutes(), time.getSeconds());
+
+useEffect(() => {
+    setInterval(() => {
+        setTime(new Date())
+    }, 1000)
+}, [])
+
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <div className=" p-4 rounded-md bg-zinc-900">
+          <div className="text-8xl font-bold flex gap-5">
+            <span className=" p-4 rounded-xl bg-zinc-950">{time.getHours()}</span>
+            <span>:</span>
+            <span className=" p-4 rounded-xl bg-zinc-950">{time.getMinutes()}</span>
+            <span>:</span>
+            <span className=" p-4 rounded-xl bg-zinc-950">{time.getSeconds()}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DigitalClock;
