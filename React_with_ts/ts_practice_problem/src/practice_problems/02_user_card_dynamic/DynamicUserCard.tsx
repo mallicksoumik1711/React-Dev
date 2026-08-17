d21d52da064a896c161b66dad1b9a798eda78d43
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-17 19:52:24 +0530

    id based changes

--- React_with_ts/ts_practice_problem/src/practice_problems/02_user_card_dynamic/DynamicUserCard.tsx
+++ React_with_ts/ts_practice_problem/src/practice_problems/02_user_card_dynamic/DynamicUserCard.tsx
@@ -5,34 +5,53 @@ type UsersList = {
     imgUrl: string,
     name: string,
     description: string,
-    isActive: true
+    isActive: boolean
 }
 
 function DynamicUserCard() {
     const [spots, setSpots] = useState<UsersList[]>([
         {
             id: 1,
-            imgUrl: "imgurl",
+            imgUrl: "https://images.pexels.com/photos/32730210/pexels-photo-32730210.jpeg",
             name: "Nature",
             description: "description",
             isActive: true
-        }
+        },
+        {
+            id: 2,
+            imgUrl: "https://images.pexels.com/photos/32730210/pexels-photo-32730210.jpeg",
+            name: "Nature",
+            description: "description",
+            isActive: true
+        },
     ])
+
+    const clickHandler = (id: number) => {
+        setSpots((prev) => {
+            // console.log(prev)
+            return prev.map((spot) => {
+                console.log(spot.isActive)
+                return id === spot.id ? { ...spot, isActive: !spot.isActive } : spot
+            })
+        })
+    }
     return (
         <>
-            <div className="flex flex-wrap p-10">
-                <div className="bg-zinc-900 overflow-hidden rounded-md w-1/4">
-                    <div className="h-50">
-                        <img className="h-full w-full object-cover" src="https://images.pexels.com/photos/32730210/pexels-photo-32730210.jpeg" alt="" />
-                    </div>
-                    <div className="p-5">
-                        <h1 className="text-2xl font-bold mb-2">Name</h1>
-                        <p className="word-wrap mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum culpa provident maiores architecto rerum earum expedita optio exercitationem nulla nihil!</p>
-                        <div>
-                            <button className="bg-black px-10 py-2 w-full rounded-full">isActive</button>
+            <div className="flex flex-wrap gap-10 p-10">
+                {spots.map((spot) => (
+                    <div key={spot.id} className={`overflow-hidden rounded-md w-1/4 ${spot.isActive ? "bg-zinc-900" : "bg-green-900"}`}>
+                        <div className="h-50">
+                            <img className="h-full w-full object-cover" src={spot.imgUrl} alt="" />
+                        </div>
+                        <div className="p-5">
+                            <h1 className="text-2xl font-bold mb-2">{spot.name}</h1>
+                            <p className="word-wrap mb-4">{spot.description}</p>
+                            <div>
+                                <button onClick={() => clickHandler(spot.id)} className="bg-black px-10 py-2 w-full rounded-full">isActive</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         </>
     )
