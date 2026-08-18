272abae67c85f4c8ab517916e90afccb3a964286
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-18 12:22:52 +0530

    card fixed

--- React_with_ts/ts_practice_problem/src/practice_problems/03_todo_state_localstorage/TodoLocalStorage.tsx
+++ React_with_ts/ts_practice_problem/src/practice_problems/03_todo_state_localstorage/TodoLocalStorage.tsx
@@ -2,17 +2,17 @@ function TodoLocalStorage() {
     return (
         <>
             <div className="flex">
-                <div className="w-1/2 p-10 bg-red-900">
+                <div className="w-1/2 p-10 ">
                     <h1 className="text-2xl font-bold mb-10">Enter tasks</h1>
                     <form className="flex flex-col flex-wrap gap-10">
                         <input type="text" placeholder="Enter task name" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700" />
                         <input type="text" placeholder="Enter img url" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700" />
                         <textarea placeholder="Enter task description" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700 resize-none"></textarea>
-                        <button className="bg-black py-4 rounded-full">Submit</button>
+                        <button className="bg-zinc-950 py-4 rounded-full">Submit</button>
                     </form>
                 </div>
-                <div className="w-1/2 p-10 flex flex-wrap gap-5 bg-yellow-900">
-                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                <div className="w-1/2 p-10 flex flex-wrap gap-5 ">
+                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
                         <div className="h-60">
                             <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
                         </div>
@@ -25,7 +25,7 @@ function TodoLocalStorage() {
                             <button>DELETE</button>
                         </div>
                     </div>
-                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
                         <div className="h-60">
                             <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
                         </div>
@@ -38,7 +38,7 @@ function TodoLocalStorage() {
                             <button>DELETE</button>
                         </div>
                     </div>
-                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
                         <div className="h-60">
                             <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
                         </div>
@@ -51,7 +51,7 @@ function TodoLocalStorage() {
                             <button>DELETE</button>
                         </div>
                     </div>
-                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
                         <div className="h-60">
                             <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
                         </div>
