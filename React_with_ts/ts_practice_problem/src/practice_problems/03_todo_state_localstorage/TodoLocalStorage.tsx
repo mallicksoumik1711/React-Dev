b1ce904dde104a57e60cc57fcb0fd6353446210b
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-18 13:56:32 +0530

    cards layout

--- React_with_ts/ts_practice_problem/src/practice_problems/03_todo_state_localstorage/TodoLocalStorage.tsx
+++ React_with_ts/ts_practice_problem/src/practice_problems/03_todo_state_localstorage/TodoLocalStorage.tsx
@@ -11,7 +11,7 @@ function TodoLocalStorage() {
                         <button className="bg-zinc-950 py-4 rounded-full">Submit</button>
                     </form>
                 </div>
-                <div className="w-1/2 p-10 flex flex-wrap gap-5 ">
+                <div className="w-1/2 p-10 flex flex-wrap gap-10 h-fit">
                     <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
                         <div className="h-60">
                             <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
@@ -20,7 +20,7 @@ function TodoLocalStorage() {
                             <h1 className="text-xl">Name</h1>
                             <p>desc</p>
                         </div>
-                        <div className="px-5 py-2 flex justify-between">
+                        <div className="px-5 py-3 flex justify-between">
                             <button>EDIT</button>
                             <button>DELETE</button>
                         </div>
@@ -33,33 +33,7 @@ function TodoLocalStorage() {
                             <h1 className="text-xl">Name</h1>
                             <p>desc</p>
                         </div>
-                        <div className="px-5 py-2 flex justify-between">
-                            <button>EDIT</button>
-                            <button>DELETE</button>
-                        </div>
-                    </div>
-                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
-                        <div className="h-60">
-                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                        </div>
-                        <div className="p-5">
-                            <h1 className="text-xl">Name</h1>
-                            <p>desc</p>
-                        </div>
-                        <div className="px-5 py-2 flex justify-between">
-                            <button>EDIT</button>
-                            <button>DELETE</button>
-                        </div>
-                    </div>
-                    <div className="bg-zinc-950 w-1/3 overflow-hidden rounded-xl">
-                        <div className="h-60">
-                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
-                        </div>
-                        <div className="p-5">
-                            <h1 className="text-xl">Name</h1>
-                            <p>desc</p>
-                        </div>
-                        <div className="px-5 py-2 flex justify-between">
+                        <div className="px-5 py-3 flex justify-between">
                             <button>EDIT</button>
                             <button>DELETE</button>
                         </div>
