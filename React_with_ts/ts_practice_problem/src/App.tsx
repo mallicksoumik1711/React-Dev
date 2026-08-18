a564485a26c7cbbc88028237f5d60df89f184628
mallicksoumik1711 <soumikmallick1711@gmail.com>
2026-08-18 12:18:50 +0530

    Todo application in ts with localstorage

--- React_with_ts/ts_practice_problem/src/App.tsx
+++ React_with_ts/ts_practice_problem/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import DynamicUserCard from "./practice_problems/02_user_card_dynamic/DynamicUserCard";
+import TodoLocalStorage from "./practice_problems/03_todo_state_localstorage/TodoLocalStorage";
+// import DynamicUserCard from "./practice_problems/02_user_card_dynamic/DynamicUserCard";
 // import Counter from "./practice_problems/01_counter_basic_types/Counter";
 
 function App() {
   return (
     <>
       {/* <Counter /> */}
-      <DynamicUserCard />
+      {/* <DynamicUserCard /> */}
+      <TodoLocalStorage />
     </>
   );
 }
---
+++ React_with_ts/ts_practice_problem/src/practice_problems/03_todo_state_localstorage/TodoLocalStorage.tsx
@@ -0,0 +1,73 @@
+function TodoLocalStorage() {
+    return (
+        <>
+            <div className="flex">
+                <div className="w-1/2 p-10 bg-red-900">
+                    <h1 className="text-2xl font-bold mb-10">Enter tasks</h1>
+                    <form className="flex flex-col flex-wrap gap-10">
+                        <input type="text" placeholder="Enter task name" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700" />
+                        <input type="text" placeholder="Enter img url" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700" />
+                        <textarea placeholder="Enter task description" className="bg-transparent block w-full px-10 py-4 rounded-full outline-none border-2 border-zinc-700 resize-none"></textarea>
+                        <button className="bg-black py-4 rounded-full">Submit</button>
+                    </form>
+                </div>
+                <div className="w-1/2 p-10 flex flex-wrap gap-5 bg-yellow-900">
+                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                        <div className="h-60">
+                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
+                        </div>
+                        <div className="p-5">
+                            <h1 className="text-xl">Name</h1>
+                            <p>desc</p>
+                        </div>
+                        <div className="px-5 py-2 flex justify-between">
+                            <button>EDIT</button>
+                            <button>DELETE</button>
+                        </div>
+                    </div>
+                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                        <div className="h-60">
+                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
+                        </div>
+                        <div className="p-5">
+                            <h1 className="text-xl">Name</h1>
+                            <p>desc</p>
+                        </div>
+                        <div className="px-5 py-2 flex justify-between">
+                            <button>EDIT</button>
+                            <button>DELETE</button>
+                        </div>
+                    </div>
+                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                        <div className="h-60">
+                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
+                        </div>
+                        <div className="p-5">
+                            <h1 className="text-xl">Name</h1>
+                            <p>desc</p>
+                        </div>
+                        <div className="px-5 py-2 flex justify-between">
+                            <button>EDIT</button>
+                            <button>DELETE</button>
+                        </div>
+                    </div>
+                    <div className="bg-green-900 w-1/3 overflow-hidden rounded-xl">
+                        <div className="h-60">
+                            <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1773332585754-f1436987743b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
+                        </div>
+                        <div className="p-5">
+                            <h1 className="text-xl">Name</h1>
+                            <p>desc</p>
+                        </div>
+                        <div className="px-5 py-2 flex justify-between">
+                            <button>EDIT</button>
+                            <button>DELETE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TodoLocalStorage
